998feec3eec0bb52a84f13400e153a24e009de7f
jailcode <pradyundangwal@gmail.com>
2025-11-22 16:11:48 +0100

    kinda compiles

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "../includes/pid.h"
 
 
-double get_current_time()
+long long get_current_time()
 {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
@@ -46,21 +46,9 @@ int main(void)
 
     error = init_error(3);
     pid = init_pid(pid, 3);
-    double time = get_current_time();
-    
-    printf("time: %f\n", time);
+    long long time = get_current_time();
+    printf("error: %d %d %d\n", error[0], error[1], error[2]);
+    printf("time: %lld\n", time);
 
     return (0);
-}
-
-
-void    init_pid(t_pid_controller *pid)
-{
-    pid->Kp = 1.0;
-    pid->Ki = 0.0;
-    pid->Kd = 0.0;
-    pid->setpoint = 0.0;
-    pid->integral = 0.0;
-    pid->last_error = 0.0;
-    pid->output = 0.0;
 }
