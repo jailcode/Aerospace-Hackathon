c4b4c1adcbabbd2700b6e196045c629e4f0aa2d5
jailcode <pradyundangwal@gmail.com>
2025-11-22 15:55:38 +0100

    latest

--- includes/pid.h
+++ includes/pid.h
@@ -5,7 +5,8 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <time.h>
-
+#include <math.h>
+#include <fcntl.h>
 
 #ifndef PID_H
 #define PID_H
@@ -19,11 +20,21 @@ typedef struct s_pid_controller
     double integral;    // Integral term
     double last_error;  // Last error value
     double output;      // Control output
+    double time_step;   // Time step for calculations
+    double simulation; // Simulation start time
 } t_pid_controller;
 
 
+typedef struct s_error
+{
+    double error_pitch;
+    double error_roll;
+    double error_yaw;
+} t_error;
 
 
-
+//function definitions:
+void    init_pid(t_pid_controller *pid);
+void    init_error(t_error *error);
 
 #endif PID_H
--- src/main.c
+++ src/main.c
@@ -8,6 +8,48 @@ double get_current_time()
     return ts.tv_sec + ts.tv_nsec / 1e9;
 }
 
+
+int    *init_error(int size)
+{
+    int *error;
+    error = (int *)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++)
+        error[i] = 0;
+    return (error);
+}
+
+t_pid_controller *init_pid(t_pid_controller *pid)
+{
+    pid = (t_pid_controller *)malloc(sizeof(t_pid_controller));
+    pid->Kp = 1.0;
+    pid->Ki = 0.0;
+    pid->Kd = 0.0;
+    pid->setpoint = 0.0;
+    pid->integral = 0.0;
+    pid->last_error = 0.0;
+    pid->output = 0.0;
+    pid->time_step = 0.01; // Example time step
+    //pid->simulation = get_current_time();
+    return (pid);
+}
+
+int main(void)
+{
+    t_pid_controller *pid;
+    int *error;
+
+    error = init_error(3);
+    pid = init_pid(pid);
+    init_pid(&pid);
+    double time = get_current_time();
+    
+    printf("time: %f\n", time);
+    start_loop()
+
+    return (0);
+}
+
+
 void    init_pid(t_pid_controller *pid)
 {
     pid->Kp = 1.0;
@@ -19,9 +61,9 @@ void    init_pid(t_pid_controller *pid)
     pid->output = 0.0;
 }
 
-int main(void)
+void    init_error(t_error *error)
 {
-    double time = get_current_time();
-    printf("Current time: %f seconds\n", time);
-    return (0);
+    error->error_pitch = 0.0;
+    error->error_roll = 0.0;
+    error->error_yaw = 0.0;
 }
