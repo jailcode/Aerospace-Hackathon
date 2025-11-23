99f75acad0b6e318f2cbab728f557d74410deaae
Pradyun Dangwal <pdangwal@c4a14c10.42berlin.de>
2025-11-23 01:06:03 +0100

    normalising

--- src/main.c
+++ src/main.c
@@ -7,6 +7,11 @@ double	Kp = 1.0f;
 double	Ki = 0.0f;
 double	Kd = 0.1f;
 
+#define ENGINE_THRUST 30;
+
+double	h_COM = 0.561;
+// we need to define a function for this that calculates values as it changes
+// need a function for normalising a given value to -1 and 1
 double	PID(int axis, double setpoint, double measurement, double dt)
 {
 	double	error;
@@ -25,36 +30,43 @@ double	PID(int axis, double setpoint, double measurement, double dt)
 	return (P + I + D);
 }
 
-void	run_loop(float dt)
+
+double  normalise(double u, double max_value, double lowerlimit, double upperlimit)
+{
+    
+}
+
+void	run_inner_loop(float dt)
 {
 	t_sensor_data	data;
 	FILE			*gyro_file;
 	FILE			*accel_file;
 	double			setpoint[3] = {0.0, 0.0, 0.0};
 	double			measurement;
-	double			output;
-
+	double			u;
+    double          MAX_u_value; //we need this for normalising it
 	if (dt <= 0)
 		dt = 0.01;
 	gyro_file = open_sensor_file("gyroscope.txt", "r");
 	accel_file = open_sensor_file("accelerometer.txt", "r");
 	if (!gyro_file || !accel_file)
 		return ;
-    
 	while (read_sensor_line(gyro_file, accel_file, &data))
 	{
 		for (int axis = 0; axis < 3; axis++)
 		{
 			measurement = data.gyro[axis];
-			output = PID(axis, setpoint[axis], measurement, dt);
-			printf("Axis %d: gyro=%f PID output=%f\n", axis, measurement,
-				output);
+			u = PID(axis, setpoint[axis], measurement, dt);
+            u = normalise(u, MAX_u_value, -1, 1);
+			printf("Axis %d: gyro=%f PID output=%f\n", axis, measurement, u);
 		}
 		usleep((unsigned int)(dt * 1000000));
 	}
 	fclose(gyro_file);
 	fclose(accel_file);
 }
+
+
 
 int	main(void)
 {
@@ -67,6 +79,7 @@ int	main(void)
 	dt = 0.1;
 	// Run the main control loop
 	// run_control_loop(dt);
-	run_loop(dt);
+	run_innter_loop(dt);
+		// apparently rocks use two PID loops called cascaded control
 	return (0);
 }
