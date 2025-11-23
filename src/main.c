7ccdc3fbcfc67c4d3be5bbb3d7780ea143c9374c
jailcode <pradyundangwal@gmail.com>
2025-11-23 09:42:43 +0100

    u to desired acceleration and a actuator command variable added

--- src/main.c
+++ src/main.c
@@ -11,8 +11,6 @@ double	thrust = 7890; // N
 
 int count = 0;
 
-
-
 // PID gains
 double	Kp = 1.0f; //proportional
 double	Ki = 0.0f; //integral
@@ -25,16 +23,17 @@ double	outer_setpoint[3] = {0.0, 0.0, 0.0};
 // Physical paramters
 // double	thrust = 30.0;
 double	MAX_u_value = 10.0;
+
+# define NUMBER_OF_AXES 3
+
 double	Ixx = 13.452;
 double	Iyy = 13.452;
 double	Izz = 0.847;
 double	theta_max = 0.1; // maximum deflection for gimbal in rad
 						// we start with 10 but make it smaller/bigger as testing goes on;
-double	h_COM = 0.561;
+double	h_COM = 0.0863;
 // we need to define a function for this that calculates values as it changes
-// need a function for normalising a given value to -1 and 1
 
-// need to add a filder for sensor noise
 double	PID(int current_axis, double setpoint, double measurement, double delta_time)
 {
 	double	current_error;
@@ -106,57 +105,84 @@ double	torque_to_gimbal_angle(double expected_alpha, double current_axis_MOI)
 	tau_required = expected_alpha * current_axis_MOI;
 	// gimbal produces torque so: tau = thrust * h_COM * sin(theta)
 	theta = asin(fmax(fmin(tau_required / (thrust * h_COM), 1.0), -1.0));
-	theta = saturation_check(theta, theta_max);
+	theta = saturation_check(theta, theta_max); // this is there to limit the actual gimbel angle
 	return (theta);
 }
 
-void	run_inner_loop(float delta_time)
+void open_files(FILE **gyro_file, FILE **accel_file)
+{
+    *gyro_file = open_sensor_file("gyroscope.txt", "r");
+    *accel_file = open_sensor_file("accelerometer.txt", "r");
+    if (!*gyro_file || !*accel_file)
+    {
+        fprintf(stderr, "Failed to open sensor files\n");
+        return ;
+    }
+}
+
+void calculate_inner_setpoint(int current_axis, double *inner_setpoint, t_sensor_data data)
+{
+    double			angle;
+	double			angle_error; // rename these variables so it makes sense
+    angle = data.accel[current_axis];
+	angle_error = outer_setpoint[current_axis] - angle;
+	inner_setpoint[current_axis] = angle_Kp * angle_error;
+    inner_setpoint[current_axis] = angle_Kp * (outer_setpoint[current_axis]);
+}
+
+calculate_gimbal_angle(double u, double current_axis)
+{
+    double gimbal_angle;
+    if (current_axis == 0)
+        gimbal_angle = torque_to_gimbal_angle(u, Ixx);
+    else if (current_axis == 1)
+        gimbal_angle = torque_to_gimbal_angle(u, Iyy);
+    else gimbal_angle = torque_to_gimbal_angle(u, Izz);
+    return gimbal_angle;
+}
+
+void	run_loop(float delta_time)
 {
 	t_sensor_data	data;
 	FILE			*gyro_file;
 	FILE			*accel_file;
 	double			inner_setpoint[3] = {0.0, 0.0, 0.0};
 	double			rate_measurement;
-	double			u;
-	double			angle;
-	double			angle_error;
+	double			desired_angular_acceleration;
+	double          actuator_command;
     double          gimbal_angle;
 
 	if (delta_time <= 0)
 		delta_time = 0.01;
-	gyro_file = open_sensor_file("gyroscope.txt", "r");
-	accel_file = open_sensor_file("accelerometer.txt", "r");
-	if (!gyro_file || !accel_file)
-		return ;
+    
+    open_files(&gyro_file, &accel_file); // opens the sensor files
 
 	FILE *plotter = fopen("results.csv","w");
 	fprintf(plotter,"axis,iteration,gyro_measurement,rate_command,gimbal_axis,server_motorized\n");
+
 	while (read_sensor_line(gyro_file, accel_file, &data))
 	{	
 		printf("Current Iteration: %d\n",count);
 
-		for (int current_axis = 0; current_axis < 3; current_axis++)
+		for (int current_axis = 0; current_axis < NUMBER_OF_AXES; current_axis++)
 		{
-			angle = data.accel[current_axis];
-			angle_error = outer_setpoint[current_axis] - angle;
-			inner_setpoint[current_axis] = angle_Kp * angle_error;
-			
-            rate_measurement = apply_filter_gyro(current_axis, data.gyro[current_axis]);
-			u = PID(current_axis, inner_setpoint[current_axis], rate_measurement, delta_time);
-			
-            if (current_axis == 0)
-                gimbal_angle = torque_to_gimbal_angle(u, Ixx);
-            else if (current_axis == 1)
-                gimbal_angle = torque_to_gimbal_angle(u, Iyy);
-            else gimbal_angle = torque_to_gimbal_angle(u, Izz);
 
-            u = normalise(gimbal_angle, MAX_u_value, -1, 1);
+			//outer_setpoint is how fast we want to rotate
+            //inter_setpoint is the angular acceleration we need to get there
+			calculate_inner_setpoint(current_axis, inner_setpoint, data);
+
+
+            rate_measurement = apply_filter_gyro(current_axis, data.gyro[current_axis]);
+			desired_angular_acceleration = PID(current_axis, inner_setpoint[current_axis], rate_measurement, delta_time);
+			
+            gimbal_angle = calculate_gimbal_angle(desired_angular_acceleration, current_axis);
+            actuator_command = normalise(gimbal_angle, MAX_u_value, -1, 1);
 			//u = saturation_check(u, MAX_u_value);
 
 			printf("Axis %d: gyro measurement: %f rate command: %f gimbal angle: %f servo normalized: %f\n", current_axis, rate_measurement,
-				inner_setpoint[current_axis], gimbal_angle, u);
+				inner_setpoint[current_axis], gimbal_angle, actuator_command);
 
-			fprintf(plotter,"%d,%d,%f,%f,%f,%f\n", current_axis, count, rate_measurement, inner_setpoint[current_axis],gimbal_angle,u);
+			fprintf(plotter,"%d,%d,%f,%f,%f,%f\n", current_axis, count, rate_measurement, inner_setpoint[current_axis],gimbal_angle, desired_angular_acceleration);
 			differential(rate_measurement,current_axis);
 
 		}
@@ -181,7 +207,7 @@ int	main(void)
 	// Run the main control loop
 	// run_control_loop(delta_time);
 
-	run_inner_loop(delta_time);
+	run_loop(delta_time);
 	// apparently rocks use two PID loops called cascaded control
 	return (0);
 }
--- visual.py
+++ visual.py
@@ -1,40 +1,48 @@
-from matplotlib import pyplot as mpl;
-import pandas as pd;
+from matplotlib import pyplot as mpl
+import pandas as pd
 
-data = pd.read_csv("results.csv");
+data = pd.read_csv("results.csv")
 
 fig, (ax1, ax2, ax3) = mpl.subplots(3, 1, figsize=(10, 15))
 
-axis_0_data = data[data['axis'] == 0]
-ax1.plot(axis_0_data['iteration'], axis_0_data['gyro_measurement'], label='Gyro Measurement', color='blue')
-ax1.plot(axis_0_data['iteration'], axis_0_data['rate_command'], label='Rate Command', color='red')
-ax1.plot(axis_0_data['iteration'], axis_0_data['gimbal_axis'], label='Gimbal Axis', color='green')
-ax1.plot(axis_0_data['iteration'], axis_0_data['server_motorized'], label='Servo', color='yellow')
-ax1.set_title('Axis 0 - Measurements, Commands & Gimbal')
-ax1.set_xlabel('Iteration')
-ax1.set_ylabel('Value')
+# ------------------------------
+# AXIS 0
+# ------------------------------
+axis_0 = data[data['axis'] == 0]
+ax1.plot(axis_0['iteration'], axis_0['gyro_measurement'], label='Gyro Measurement')
+ax1.plot(axis_0['iteration'], axis_0['rate_command'], label='Rate Command')
+ax1.plot(axis_0['iteration'], axis_0['gimbal_axis'], label='Gimbal Axis')
+ax1.plot(axis_0['iteration'], axis_0['server_motorized'], label='Servo')
+ax1.set_title("Axis 0 - Raw Data")
+ax1.set_xlabel("Iteration")
+ax1.set_ylabel("Raw Value")
 ax1.legend()
 
-axis_1_data = data[data['axis'] == 1]
-ax2.plot(axis_1_data['iteration'], axis_1_data['gyro_measurement'], label='Gyro Measurement', color='blue')
-ax2.plot(axis_1_data['iteration'], axis_1_data['rate_command'], label='Rate Command', color='red')
-ax2.plot(axis_1_data['iteration'], axis_1_data['gimbal_axis'], label='Gimbal Axis', color='green')
-ax2.plot(axis_1_data['iteration'], axis_1_data['server_motorized'], label='Servo', color='yellow')
-ax2.set_title('Axis 1 - Measurements, Commands & Gimbal')
-ax2.set_xlabel('Iteration')
-ax2.set_ylabel('Value')
+# ------------------------------
+# AXIS 1
+# ------------------------------
+axis_1 = data[data['axis'] == 1]
+ax2.plot(axis_1['iteration'], axis_1['gyro_measurement'], label='Gyro Measurement')
+ax2.plot(axis_1['iteration'], axis_1['rate_command'], label='Rate Command')
+ax2.plot(axis_1['iteration'], axis_1['gimbal_axis'], label='Gimbal Axis')
+ax2.plot(axis_1['iteration'], axis_1['server_motorized'], label='Servo')
+ax2.set_title("Axis 1 - Raw Data")
+ax2.set_xlabel("Iteration")
+ax2.set_ylabel("Raw Value")
 ax2.legend()
 
-axis_2_data = data[data['axis'] == 2]
-ax3.plot(axis_2_data['iteration'], axis_2_data['gyro_measurement'], label='Gyro Measurement', color='blue')
-ax3.plot(axis_2_data['iteration'], axis_2_data['rate_command'], label='Rate Command', color='red')
-ax3.plot(axis_2_data['iteration'], axis_2_data['gimbal_axis'], label='Gimbal Axis', color='green')
-ax3.plot(axis_2_data['iteration'], axis_2_data['server_motorized'], label='Servo', color='yellow')
-ax3.set_title('Axis 2 - Measurements, Commands & Gimbal')
-ax3.set_xlabel('Iteration')
-ax3.set_ylabel('Value')
+# ------------------------------
+# AXIS 2
+# ------------------------------
+axis_2 = data[data['axis'] == 2]
+ax3.plot(axis_2['iteration'], axis_2['gyro_measurement'], label='Gyro Measurement')
+ax3.plot(axis_2['iteration'], axis_2['rate_command'], label='Rate Command')
+ax3.plot(axis_2['iteration'], axis_2['gimbal_axis'], label='Gimbal Axis')
+ax3.plot(axis_2['iteration'], axis_2['server_motorized'], label='Servo')
+ax3.set_title("Axis 2 - Raw Data")
+ax3.set_xlabel("Iteration")
+ax3.set_ylabel("Raw Value")
 ax3.legend()
 
 fig.tight_layout()
-
 mpl.show()
